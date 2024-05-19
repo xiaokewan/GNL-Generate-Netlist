31169d92e5e542199c4e9d624e23eeba1360a21f
xiaokewan <xiaoke.wang@ugent.be>
2024-05-19 22:39:57 +0200

    modified - compvpr

--- post_process/compvpr.py
+++ post_process/compvpr.py
@@ -116,9 +116,269 @@ import numpy as np
 #     plt.show()
 
 
-def draw_fit_in_diff_files(filenames, columns_to_fit, rent_length=None):
-    def exponential_func(x, a, b, c):
-        return a * np.exp(b * x) + c
+def extract_number(filename):
+    numbers = re.findall(r'\d+', filename)
+    return int(numbers[0]) if numbers else 0
+
+
+def linear_func(x, a, b):
+    return a * x + b
+
+
+def exponential_func(x, a, b, c):
+    return a * np.exp(b * x) + c
+
+
+y_axis_labels = {
+    'time': 'Time (sec)',
+    'cpd': 'CPD (ns)',
+    'total_wirelength': 'Total Wirelength',
+    'packing_time': 'Packing Time (sec)',
+    'routing_time': 'Routing Time (sec)',
+    'placement_time': 'Placement Time (sec)'
+}
+
+
+def draw_double_log_fit_in_diff_files(filenames, columns_to_fit, rent_length=None, savefigure=True):
+    sorted_filenames = sorted(filenames, key=extract_number)
+
+    fig, axs = plt.subplots(len(columns_to_fit), 1, figsize=(10, 6 * len(columns_to_fit)))
+    plt.rcParams.update({
+        "text.usetex": True,
+        "text.latex.preamble": r"\usepackage{xcolor}"
+    })
+    if not isinstance(axs, np.ndarray):
+        axs = [axs]
+
+    # Create a colormap instance
+    cmap = plt.colormaps.get_cmap('Set1')
+    main_directory = os.path.abspath(os.path.join(os.path.dirname(filenames[0]), '..', '..'))
+    pics_directory = os.path.join(main_directory, 'pics')
+
+    for i, column in enumerate(columns_to_fit):
+        fitted_values_region = []
+        rent_exp = []
+
+        for j, filename in enumerate(sorted_filenames):
+            data = pd.read_csv(filename)
+            sorted_data = data.sort_values(by='rent_exp')
+            clean_data = sorted_data.dropna(subset=[column])
+            numbers = re.findall(r'\d+', os.path.basename(filename))
+            rent_exp.append(clean_data['rent_exp'].values)
+            number = numbers[0] if numbers else os.path.basename(filename)
+            if clean_data.empty:
+                print(f"Warning: No data available for column '{column}' in file '{filename}'")
+                continue
+
+            if rent_length is not None:
+                rent_exp_data = clean_data['rent_exp'].values[:rent_length]
+                clean_data_value = clean_data[column].values[:rent_length]
+            else:
+                rent_exp_data = clean_data['rent_exp'].values
+                clean_data_value = clean_data[column].values
+
+            # Apply logarithm to the y-axis data and handle invalid values
+            valid_mask = (clean_data_value > 0) & (~np.isinf(clean_data_value))
+            clean_data_value = clean_data_value[valid_mask]
+            rent_exp_data = rent_exp_data[valid_mask]
+
+            log_clean_data_value = np.log(np.log(clean_data_value))
+            valid_mask = ~np.isinf(log_clean_data_value) & ~np.isnan(log_clean_data_value)
+            log_clean_data_value = log_clean_data_value[valid_mask]
+            rent_exp_data = rent_exp_data[valid_mask]
+
+            if len(log_clean_data_value) == 0:
+                print(f"Warning: All data for column '{column}' in file '{filename}' resulted in invalid log values.")
+                continue
+            label = f'Blocks: ${number}$'
+            axs[i].scatter(rent_exp_data, log_clean_data_value, color=cmap(j), label=label)
+            try:
+                if rent_length is not None:
+                    popt, _ = curve_fit(linear_func, rent_exp_data[:rent_length],
+                                        log_clean_data_value[:rent_length])
+                    fitted_values = linear_func(rent_exp_data[:rent_length], *popt)
+                else:
+                    popt, _ = curve_fit(linear_func, rent_exp_data, log_clean_data_value)
+                    fitted_values = linear_func(rent_exp_data, *popt)
+
+                fitted_values_region.append(fitted_values)
+
+                a, b = popt
+                a_sci = f"{a:.2e}".split('e')  # Split into base and exponent
+                a_base, a_exp = a_sci[0], a_sci[1]
+                # label = f'Blocks: ${number}$\n Fit: $\\ln(\\ln(y)) =  ({a_base} \\times 10^{{{a_exp}}})x + {b:.2f}$'
+                #
+                color = cmap(j)  # Use colormap to determine the color
+
+                # axs[i].plot(rent_exp_data, fitted_values, label=label, color=color, linewidth=3, alpha=0.7)
+
+            except RuntimeError:
+                print(f"Error: Unable to fit linear curve for column '{column}' in file '{filename}'")
+                continue
+
+        if fitted_values_region:
+            shortest_length = min(len(values) for values in fitted_values_region)
+            if rent_length is not None and rent_length <= shortest_length:
+                shortest_length = rent_length
+        else:
+            print("Not enough data points were successfully fitted to compare lengths.")
+            shortest_length = None
+
+        resized_fitted_values = []
+        for values in fitted_values_region:
+            resized_values = values[:shortest_length]  # Keep only the first 'shortest_length' elements
+            resized_fitted_values.append(resized_values)
+
+        # for k in range(len(resized_fitted_values) - 1):
+        #     axs[i].fill_between(max(rent_exp, key=len)[:shortest_length], resized_fitted_values[k],
+        #                         resized_fitted_values[k + 1],
+        #                         color=cmap(k), alpha=0.3)
+
+        # Further plotting logic and setup as in your original code
+        y_label = y_axis_labels.get(column, f'Log(Log({column}))')
+        axs[i].set_xlabel('Rent Exponent')
+        axs[i].set_ylabel(f'Log(Log({column}))')
+        axs[i].set_title(f'Double Logarithmic scale for {column}')
+        axs[i].legend()
+        axs[i].grid(True)
+
+        individual_plot_path = os.path.join(pics_directory, f'{column}_vpr_flow_log_log.png')
+        fig_individual, ax_individual = plt.subplots()
+        ax_individual.scatter(rent_exp_data, log_clean_data_value, color=cmap(i), label=label)
+        # if fitted_values_region:
+        #     ax_individual.plot(rent_exp_data, fitted_values, color=color, linewidth=3, alpha=0.7)
+        ax_individual.set_xlabel('Rent Exponent')
+        ax_individual.set_ylabel(y_label)
+        ax_individual.set_title(f'Double Logarithmic scale for {column}')
+        ax_individual.legend()
+        ax_individual.grid(True)
+        if savefigure:
+            fig_individual.savefig(individual_plot_path)
+        plt.close(fig_individual)
+    plt.tight_layout()
+    if savefigure:
+
+        plt.savefig(os.path.join(pics_directory, 'rent_exp_influence_vpr_flow_log_log.png'))
+    plt.show()
+
+
+def draw_log_fit_in_diff_files(filenames, columns_to_fit, rent_length=None, savefigure=True):
+    sorted_filenames = sorted(filenames, key=extract_number)
+
+    fig, axs = plt.subplots(len(columns_to_fit), 1, figsize=(10, 6 * len(columns_to_fit)))
+    plt.rcParams.update({
+        "text.usetex": True,
+        "text.latex.preamble": r"\usepackage{xcolor}"
+    })
+    if not isinstance(axs, np.ndarray):
+        axs = [axs]
+
+    # Create a colormap instance
+    cmap = plt.colormaps.get_cmap('Set1')
+    main_directory = os.path.abspath(os.path.join(os.path.dirname(filenames[0]), '..', '..'))
+    pics_directory = os.path.join(main_directory, 'pics')
+
+    for i, column in enumerate(columns_to_fit):
+        fitted_values_region = []
+        rent_exp = []
+
+        for j, filename in enumerate(sorted_filenames):
+            data = pd.read_csv(filename)
+            sorted_data = data.sort_values(by='rent_exp')
+            clean_data = sorted_data.dropna(subset=[column])
+            numbers = re.findall(r'\d+', os.path.basename(filename))
+            rent_exp.append(clean_data['rent_exp'].values)
+            number = numbers[0] if numbers else os.path.basename(filename)
+            if clean_data.empty:
+                print(f"Warning: No data available for column '{column}' in file '{filename}'")
+                continue
+
+            if rent_length is not None:
+                rent_exp_data = clean_data['rent_exp'].values[:rent_length]
+                clean_data_value = clean_data[column].values[:rent_length]
+            else:
+                rent_exp_data = clean_data['rent_exp'].values
+                clean_data_value = clean_data[column].values
+
+            # Apply logarithm to the y-axis data
+            log_clean_data_value = np.log(clean_data_value)
+
+            axs[i].scatter(rent_exp_data, log_clean_data_value, color=cmap(j))
+            try:
+                if rent_length is not None:
+                    popt, _ = curve_fit(exponential_func, clean_data['rent_exp'].iloc[:rent_length],
+                                        log_clean_data_value[:rent_length])
+                    fitted_values = exponential_func(clean_data['rent_exp'].iloc[:rent_length], *popt)
+                else:
+                    popt, _ = curve_fit(exponential_func, clean_data['rent_exp'], log_clean_data_value)
+                    fitted_values = exponential_func(clean_data['rent_exp'], *popt)
+
+                fitted_values_region.append(fitted_values)
+
+                a, b, c = popt
+                a_sci = f"{a:.2e}".split('e')
+                a_base, a_exp = a_sci[0], a_sci[1]
+                # label = f'Blocks: {number}\nFit: $y = ({a:.2e}) \\cdot e^{{-b:.2f \\cdot x}} + {c:.2f}$'
+                label = f'Blocks: ${number}$\n Fit: $y =  ({a_base} \\times 10^{{{a_exp}}})\\cdot e^{{{b:.2f} \\cdot x}} + {c:.2f}$'
+
+                color = cmap(j)  # Use colormap to determine the color
+
+                axs[i].plot(rent_exp_data, fitted_values, label=label, color=color, linewidth=3, alpha=0.7)
+
+
+            except RuntimeError:
+                print(f"Error: Unable to fit linear curve for column '{column}' in file '{filename}'")
+                continue
+
+        if fitted_values_region:
+            shortest_length = min(len(values) for values in fitted_values_region)
+            if rent_length is not None and rent_length <= shortest_length:
+                shortest_length = rent_length
+        else:
+            print("Not enough data points were successfully fitted to compare lengths.")
+            shortest_length = None
+
+        resized_fitted_values = []
+        for values in fitted_values_region:
+            resized_values = values[:shortest_length]  # Keep only the first 'shortest_length' elements
+            resized_fitted_values.append(resized_values)
+
+        for k in range(len(resized_fitted_values) - 1):
+            axs[i].fill_between(max(rent_exp, key=len)[:shortest_length], resized_fitted_values[k],
+                                resized_fitted_values[k + 1],
+                                color=cmap(k), alpha=0.3)
+
+        # Further plotting logic and setup as in your original code
+        y_label = y_axis_labels.get(column, f'Log(Log({column}))')
+        axs[i].set_xlabel('Rent Exponent')
+        axs[i].set_ylabel('Log(Value)')
+        axs[i].set_title(f'Logarithmic Fit for {column}')
+        axs[i].legend()
+        axs[i].grid(True)
+
+        # Save each sub-figure individually
+        individual_plot_path = os.path.join(pics_directory, f'{column}_vpr_flow_log.png')
+        fig_individual, ax_individual = plt.subplots()
+        ax_individual.scatter(rent_exp_data, log_clean_data_value, color=cmap(i), label=label)
+        if fitted_values_region:
+            ax_individual.plot(rent_exp_data, fitted_values, color=cmap(i), linewidth=3, alpha=0.7)
+        ax_individual.set_xlabel('Rent Exponent')
+        ax_individual.set_ylabel(y_label)
+        ax_individual.set_title(f'Logarithmic scale for {column}')
+        ax_individual.legend()
+        ax_individual.grid(True)
+        if savefigure:
+            fig_individual.savefig(individual_plot_path)
+        plt.close(fig_individual)
+
+    plt.tight_layout()
+    if savefigure:
+        plt.savefig(os.path.join(pics_directory, 'rent_exp_influence_vpr_flow_log.png'))
+    plt.show()
+
+
+def draw_fit_in_diff_files(filenames, columns_to_fit, rent_length=None, savefigure=True):
+
 
     sorted_filenames = sorted(filenames, key=extract_number)
 
@@ -132,6 +392,8 @@ def draw_fit_in_diff_files(filenames, columns_to_fit, rent_length=None):
 
     # Create a colormap instance
     cmap = plt.colormaps.get_cmap('Set1')  # 'viridis' with as many colors as there are files
+    main_directory = os.path.abspath(os.path.join(os.path.dirname(filenames[0]), '..', '..'))
+    pics_directory = os.path.join(main_directory, 'pics')
 
     for i, column in enumerate(columns_to_fit):
         fitted_values_region = []
@@ -167,7 +429,7 @@ def draw_fit_in_diff_files(filenames, columns_to_fit, rent_length=None):
                 fitted_values_region.append(fitted_values)
 
                 a, b, c = popt
-                a_sci = f"{a:.2e}".split('e')  # 分解为基数和指数
+                a_sci = f"{a:.2e}".split('e')
                 a_base, a_exp = a_sci[0], a_sci[1]
                 # label = f'Blocks: {number}\nFit: $y = ({a:.2e}) \\cdot e^{{-b:.2f \\cdot x}} + {c:.2f}$'
                 label = f'Blocks: ${number}$\n Fit: $y =  ({a_base} \\times 10^{{{a_exp}}})\\cdot e^{{{b:.2f} \\cdot x}} + {c:.2f}$'
@@ -182,7 +444,6 @@ def draw_fit_in_diff_files(filenames, columns_to_fit, rent_length=None):
 
         if fitted_values_region:
             shortest_length = min(len(values) for values in fitted_values_region)
-            print(f"shot length: {shortest_length}")
             if rent_length is not None and rent_length <= shortest_length:
                 shortest_length = rent_length
         else:
@@ -192,23 +453,36 @@ def draw_fit_in_diff_files(filenames, columns_to_fit, rent_length=None):
         for values in fitted_values_region:
             resized_values = values[:shortest_length]  # Keep only the first 'shortest_length' elements
             resized_fitted_values.append(resized_values)
-        print(resized_fitted_values)
         for k in range(len(resized_fitted_values) - 1):
             axs[i].fill_between(max(rent_exp, key=len)[:shortest_length], resized_fitted_values[k],
                                 resized_fitted_values[k + 1],
                                 color=cmap(k), alpha=0.3)
 
         # Further plotting logic and setup as in your original code
+        y_label = y_axis_labels.get(column, f'Log(Log({column}))')
         axs[i].set_xlabel('Rent Exponent')
-        axs[i].set_ylabel('Value')
+        axs[i].set_ylabel(f'{column}')
         axs[i].set_title(f'Exponential Fit for {column}')
         axs[i].legend()
         axs[i].grid(True)
 
+        # Save each sub-figure individually
+        individual_plot_path = os.path.join(pics_directory, f'{column}_vpr_flow.png')
+        fig_individual, ax_individual = plt.subplots()
+        ax_individual.scatter(rent_exp_data, clean_data_value, color=cmap(i), label=label)
+        if fitted_values_region:
+            ax_individual.plot(rent_exp_data, fitted_values, color=cmap(i), linewidth=3, alpha=0.7)
+        ax_individual.set_xlabel('Rent Exponent')
+        ax_individual.set_ylabel(y_label)
+        ax_individual.set_title(f'{column}')
+        ax_individual.legend()
+        ax_individual.grid(True)
+        if savefigure:
+            fig_individual.savefig(individual_plot_path)
+        plt.close(fig_individual)
     plt.tight_layout()
-    directory = os.path.dirname(filenames[0])
-
-    plt.savefig(os.path.join(directory, 'rent_exp_influence2vpr_flow.png'))
+    if savefigure:
+        plt.savefig(os.path.join(pics_directory, 'rent_exp_influence2vpr_flow.png'))
     plt.show()
 
 
@@ -243,6 +517,12 @@ if __name__ == '__main__':
 
     # Example function call, assuming this function is defined elsewhere
     draw_fit_in_diff_files(csv_files,
-                           columns_to_fit=['time', 'cpd', 'total_wirelength', 'estimate_distance_1', 'estimate_distance_2',
+                           columns_to_fit=['time', 'cpd', 'total_wirelength',
                                            'packing_time', 'routing_time', 'placement_time'], rent_length=33)
-    # draw_fit_in_diff_files(csv_files, columns_to_fit=['estimate_distance'], rent_length=33)
+    draw_log_fit_in_diff_files(csv_files,
+                           columns_to_fit=['time', 'cpd', 'total_wirelength',
+                                           'packing_time', 'routing_time', 'placement_time'], rent_length=33)
+    # # draw_fit_in_diff_files(csv_files, columns_to_fit=['estimate_distance'], rent_length=33)
+    draw_double_log_fit_in_diff_files(csv_files,
+                           columns_to_fit=['time', 'cpd', 'total_wirelength',
+                                           'packing_time', 'routing_time', 'placement_time'], rent_length=33)
